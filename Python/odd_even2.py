0042f945f67fd1daf0c5fcb5715a186e91e53f7c
MD. Fazle Rabbi Riyad <135745417+RiyadAndroidDev@users.noreply.github.com>
2024-02-13 12:54:20 +0600

    New Code Added

--- Python/odd_even2.py
+++ Python/odd_even2.py
@@ -2,17 +2,18 @@ def checker(value):
     if value % 2 == 0:
         return True
     else:
-        return False 
+        return False
+
 
 odd = []
 even = []
 limit = int(input("Enter the range: "))
-for i in range(1, limit+1):
+for i in range(1, limit + 1):
     if checker(i):
         odd.append(i)
-    else:
-        even.append(i)
-
+else:
+    even.append(i)
+"""riyad"""
 print(odd)
 print(even)
 print("Finished")
