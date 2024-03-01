b998d42bc6012bfaf04d586ae2053581c08463d7
MD. Fazle Rabbi Riyad <135745417+RiyadAndroidDev@users.noreply.github.com>
2024-03-01 11:40:39 +0600

    New Code Added

--- Python/class_object2.py
+++ Python/class_object2.py
@@ -7,4 +7,4 @@ obj1 = student()
 print(isinstance(obj1, student))
 obj1.roll = 213
 obj1.name = "Riyad"
-print(f"My name is {obj1.name} and my roll is {obj1.roll}")
+print(f"My name is {obj1.name} and my roll is {obj1.roll}")  
--- Python/construction.py
+++ Python/construction.py
@@ -1,14 +1,31 @@
-class teacher:
-    def __init__(self, a, b, c):
-        self.name = a
-        self.id = b
-        self.salary = c
+# class teacher:
+#     def __init__(self, a, b, c):
+#         self.name = a
+#         self.id = b
+#         self.salary = c
 
-    def display(self):
-        print(self.name)
-        print(self.id)
-        print(self.salary)
+#     def display(self):
+#         print(self.name)
+#         print(self.id)
+#         print(self.salary)
 
 
-obj1 = teacher("Riyad", 2027, 1000)
-obj1.display()
+# obj1 = teacher("Riyad", 2027, 1000)
+# obj1.display()
+
+
+# Area of a Triangle
+class Triangle:
+    def __init__(self, tbase, theight):
+        self.base = tbase
+        self.height = theight
+
+    def ans(self):
+        area = 0.5 * self.base * self.height
+        print("Area = ", area)
+
+
+obj1 = Triangle(10, 20)
+obj1.ans()
+obj2 = Triangle(15, 20)
+obj2.ans()
