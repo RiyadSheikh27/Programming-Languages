9e07be862db4203a77ac8d53a6607ffbf4aa192e
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2025-02-26 23:31:21 +0600

    New problem solved for practice

--- Python/Problem_solving/armstrong2.py
+++ Python/Problem_solving/armstrong2.py
@@ -1,3 +1,10 @@
-#Armstrong numbers from 100 to 1000
-for i in range(100,1000):
+for i in range(100, 1000):  
+    num_str = str(i)  
+    digits = [int(d) for d in num_str]  
     
+    arm_num = sum(d ** 3 for d in digits)  
+    
+    if arm_num == i: 
+        print(i)  
+
+
